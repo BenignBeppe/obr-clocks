f746b6da2116f08583449e66a2ad20470ea2d509
Sebastian Berlin <berlin.sebastian@gmail.com>
2025-07-17 12:29:00 +0200

    Add tool icon to tool bar

---
+++ README.md
@@ -0,0 +1,7 @@
+# OBR Clocks
+
+## Credits
+
+Vectors and icons by [SVG Repo](https://www.svgrepo.com):
+
+* public/tool.svg - [by KDE under GPL License](https://www.svgrepo.com/svg/500314/wheel-sectors).
--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,28 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import OBR from "@owlbear-rodeo/sdk";
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+    <StrictMode>
+        <App />
+    </StrictMode>,
 )
+
+const ID = "eu.sebber.obr-clock";
+
+function createTool() {
+    OBR.tool.create({
+        id: `${ID}/tool`,
+        icons: [
+            {
+                icon: "/tool.svg",
+                label: "Clocks",
+            }
+        ]
+    });
+}
+
+OBR.onReady(() => {
+    createTool();
+});
