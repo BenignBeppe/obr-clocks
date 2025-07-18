f1c0896f6a94fe7c43f2894366f2f527292c4b76
Sebastian Berlin <berlin.sebastian@gmail.com>
2025-07-18 10:24:34 +0200

    Add clock items
    Adds three tool modes for different sizes of clocks. When active, clicking
    will add a circle and a number of dividers to draw the clock.
    
    Also:
    * Removes some Vite templates.

---
+++ src/geometry.ts
@@ -0,0 +1,16 @@
+import { type Vector2 } from "@owlbear-rodeo/sdk";
+
+export function getPositionOnCircle(x: number, y: number, radius: number, degrees: number): Vector2 {
+    // https://stackoverflow.com/a/839931/1021969
+    // x = cx + r * cos(a)
+    // y = cy + r * sin(a)
+    // Where r is the radius, cx,cy the origin, and a the angle.
+    // let angle = degrees ? (360 / segments * degrees) : 0;
+    let radians = degrees * (Math.PI / 180);
+    let position = {
+        x: x + radius * Math.cos(radians),
+        y: y + radius * Math.sin(radians)
+    }
+
+    return position;
+}
---
+++ src/main.ts
@@ -0,0 +1,75 @@
+import OBR, { type Line, type ToolContext, type ToolEvent, buildLine, buildShape } from "@owlbear-rodeo/sdk";
+
+import { getPositionOnCircle } from "./geometry";
+
+const ID = "eu.sebber.obr-clock";
+
+function createTool() {
+    OBR.tool.create({
+        id: `${ID}/tool`,
+        icons: [
+            {
+                icon: "/tool.svg",
+                label: "Clocks",
+            }
+        ]
+    });
+}
+
+function createSegmentMode(segments: number, label: string) {
+    OBR.tool.createMode({
+        id: `${ID}/mode-${segments}`,
+        icons: [
+            {
+                icon: `/mode-${segments}.svg`,
+                label: label,
+                filter: {
+                    activeTools: [`${ID}/tool`],
+                }
+            }
+        ],
+        onToolClick: onToolClick
+    });
+}
+
+async function onToolClick(context: ToolContext, event: ToolEvent) {
+    // TODO: Implement nicer.
+    let segments = Number(context.activeMode?.split("-").at(-1));
+    let x = event.pointerPosition.x;
+    let y = event.pointerPosition.y;
+    let colour = "#FFF";
+    let radius = 50;
+    let item = buildShape()
+        .shapeType("CIRCLE")
+        .position({x: x, y: y})
+        .width(radius * 2)
+        .height(radius * 2)
+        .strokeColor(colour)
+        .build();
+    // We need to wait here to make sure this doesn't go over the dividers.
+    await OBR.scene.items.addItems([item]);
+
+    let dividers: Line[] = [];
+    for(let i = 0; i < segments / 2; i ++) {
+        let angle = i ? (360 / segments * i) : 0;
+        let start = getPositionOnCircle(x, y, radius, angle);
+        let end = getPositionOnCircle(x, y, radius, angle + 180);
+        let divider = buildLine()
+            .startPosition(start)
+            .endPosition(end)
+            .strokeColor(colour)
+            .build();
+        dividers.push(divider);
+    }
+    // TODO: Figure out if there is a good way to prevent the flickering that
+    // sometimes happens when drawing these. Possibly by creating items hidden
+    // and show them all at once.
+    OBR.scene.items.addItems(dividers);
+}
+
+OBR.onReady(() => {
+    createTool();
+    createSegmentMode(4, "Four segments");
+    createSegmentMode(6, "Six segments");
+    createSegmentMode(8, "Eight segments");
+});
