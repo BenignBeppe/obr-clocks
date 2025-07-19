2f62dd0b0d85b20cde257ea7cfd64c89d4f1c0ba
Sebastian Berlin <berlin.sebastian@gmail.com>
2025-07-19 09:33:56 +0200

    Toggle segments
    Double clicking or ctrl + clicking a segment toggles it filled or not. This is
    done by locking the segment items and toggling when they're selected. They're
    immediately deselected because you shouldn't be able to do anything else with
    them.

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import OBR, { Command, type Item, type ToolContext, type ToolEvent, buildLine, buildPath, buildShape } from "@owlbear-rodeo/sdk";
+import OBR, { Command, type Item, type Path, type ToolContext, type ToolEvent, buildLine, buildPath, buildShape } from "@owlbear-rodeo/sdk";
 
 import { getPositionOnCircle } from "./geometry";
 
@@ -38,12 +38,14 @@ async function addClock(context: ToolContext, event: ToolEvent) {
     let x = event.pointerPosition.x;
     let y = event.pointerPosition.y;
     let lineColour = "hsl(0 0% 75%)";
+    let segmentColour = "hsl(100 100% 30%)";
     let radius = 50;
     let base = buildShape()
         .shapeType("CIRCLE")
         .position({x: x, y: y})
-        .width(radius * 2)
-        .height(radius * 2)
+        // Add a bit border to the base to grab on to.
+        .width((radius + 10) * 2)
+        .height((radius + 10 ) * 2)
         .strokeWidth(0)
         .build();
     let items: Item[] = [base];
@@ -66,6 +68,7 @@ async function addClock(context: ToolContext, event: ToolEvent) {
         let between = getPositionOnCircle(x, y, radius, 360 / segments * (i + 0.5));
         let next = points[j];
         let segment = buildPath()
+            .name(`${ID}/segment`)
             .zIndex(base.zIndex + 1)
             // TODO: Figure out how to make this with a round edge. Four
             // segment clocks look wonky.
@@ -77,9 +80,9 @@ async function addClock(context: ToolContext, event: ToolEvent) {
                 [Command.CLOSE],
             ])
             .strokeWidth(0)
-            .fillColor("hsl(100 100% 30%)")
+            .fillColor(segmentColour)
             .attachedTo(base.id)
-            .disableHit(true)
+            .locked(true)
             .build();
         items.push(segment);
     }
@@ -120,4 +123,37 @@ OBR.onReady(() => {
     createSegmentMode(4, "Four segments");
     createSegmentMode(6, "Six segments");
     createSegmentMode(8, "Eight segments");
+
+    OBR.player.onChange(async (player) => {
+        let selectedId = player.selection;
+        if(selectedId === undefined || selectedId.length === 0) {
+            // Nothing selected.
+            return;
+        }
+
+        if(selectedId.length > 1) {
+            // Multiple items selected.
+            return;
+        }
+
+        let selectedItem = (await OBR.scene.items.getItems(selectedId))[0];
+        if(selectedItem.name !== `${ID}/segment`) {
+            // Selected item is not a segment.
+            return;
+        }
+
+        // TODO: Is there a way to avoid the selection UI popping up for a
+        // split second before it's deselected?
+        OBR.player.deselect();
+        let segment = selectedItem as Path;
+        OBR.scene.items.updateItems([segment], (items) => {
+            if(items[0].style.fillOpacity === 1) {
+                // Looks like the opacity just need to be more than zero to
+                // keep the item selectable.
+                items[0].style.fillOpacity = 0.01;
+            } else {
+                items[0].style.fillOpacity = 1;
+            }
+        });
+    });
 });
