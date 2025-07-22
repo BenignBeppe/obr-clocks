1eb49d6552d2a3f40714c9092f382832b2373008
Sebastian Berlin <berlin.sebastian@gmail.com>
2025-07-22 09:49:10 +0200

    Break out selection listener

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import OBR, { type Path, type ToolContext, type ToolEvent } from "@owlbear-rodeo/sdk";
+import OBR, { type Path, type Player, type ToolContext, type ToolEvent } from "@owlbear-rodeo/sdk";
 
 import { addClock } from "./items";
 import { ID } from "./util";
@@ -35,42 +35,44 @@ function createSegmentMode(segments: number, label: string) {
     });
 }
 
+async function handleSelect(player: Player) {
+    let selectedId = player.selection;
+    if (selectedId === undefined || selectedId.length === 0) {
+        // Nothing selected.
+        return;
+    }
+
+    if (selectedId.length > 1) {
+        // Multiple items selected.
+        return;
+    }
+
+    let selectedItem = (await OBR.scene.items.getItems(selectedId))[0];
+    if (selectedItem.name !== `${ID}/segment`) {
+        // Selected item is not a segment.
+        return;
+    }
+
+    // TODO: Is there a way to avoid the selection UI popping up for a
+    // split second before it's deselected?
+    OBR.player.deselect();
+    let segment = selectedItem as Path;
+    OBR.scene.items.updateItems([segment], (items) => {
+        if (items[0].style.fillOpacity === 1) {
+            // Looks like the opacity just need to be more than zero to
+            // keep the item selectable.
+            items[0].style.fillOpacity = 0.01;
+        } else {
+            items[0].style.fillOpacity = 1;
+        }
+    });
+}
+
 OBR.onReady(() => {
     createTool();
     createSegmentMode(4, "Four segments");
     createSegmentMode(6, "Six segments");
     createSegmentMode(8, "Eight segments");
 
-    OBR.player.onChange(async (player) => {
-        let selectedId = player.selection;
-        if(selectedId === undefined || selectedId.length === 0) {
-            // Nothing selected.
-            return;
-        }
-
-        if(selectedId.length > 1) {
-            // Multiple items selected.
-            return;
-        }
-
-        let selectedItem = (await OBR.scene.items.getItems(selectedId))[0];
-        if(selectedItem.name !== `${ID}/segment`) {
-            // Selected item is not a segment.
-            return;
-        }
-
-        // TODO: Is there a way to avoid the selection UI popping up for a
-        // split second before it's deselected?
-        OBR.player.deselect();
-        let segment = selectedItem as Path;
-        OBR.scene.items.updateItems([segment], (items) => {
-            if(items[0].style.fillOpacity === 1) {
-                // Looks like the opacity just need to be more than zero to
-                // keep the item selectable.
-                items[0].style.fillOpacity = 0.01;
-            } else {
-                items[0].style.fillOpacity = 1;
-            }
-        });
-    });
+    OBR.player.onChange(handleSelect);
 });
